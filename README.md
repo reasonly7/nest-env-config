53903de66d5f9d28dad48e2ef722faf4f70ed552
reasonly8 <reasonly8@sina.com>
2024-10-10 23:00:13 +0800

    fix: envFilePath is not working

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # nest-env-config
+
 The best practices for managing configurations in NestJS（NestJS 中管理配置的最佳实践）
+
+Step 1: install @nestjs/config
+
+```sh
+$ pnpm i @nestjs/config
+```
+
+Step 2: import ConfigModule in `app.module.ts`
+
+```ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+
+  })],
+  // ...
+})
+export class AppModule {}
+```
+
+Step 3: see 👀: [popular config]('./config')
+
+![](./config-env-file.png)
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: [
+        path.resolve(__dirname, '../config/.env.development.local'),
+        path.resolve(__dirname, '../config/.env.development'),
+        path.resolve(__dirname, '../config/.env'),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
